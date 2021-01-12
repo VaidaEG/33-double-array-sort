9a4ef9599c3f117438a94bfa9a5e0b0c6ab18adb
VaidaEG <grausliene.vaida@gmail.com>
2021-01-12 21:27:24 +0200

    Array sorting update

--- src/pkgdouble/array/sort/DoubleArraySort.java
+++ src/pkgdouble/array/sort/DoubleArraySort.java
@@ -15,36 +15,25 @@ public class DoubleArraySort {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[] masyvas = {1, 2, 3, 4, 5};
-        int[][] doubleMasyvas= new int[masyvas.length][masyvas.length];
-        int offset = 0;
-        // numbers move from left to right one number in a line
-        for (int i = 0; i < doubleMasyvas.length; i++, offset++) {
-            int[] line = doubleMasyvas[i];
-            for (int j = 0; j < line.length; j++) {
-                int temp = offset;
-                if (temp + j > line.length - 1)
-                    
-                    temp -= line.length;
-                    line[j] = masyvas[temp + j];
-                    System.out.print(line[j]);
-                    
-                
+        int[] array = {1, 2, 3, 4, 5};
+        int[][] doubleArray= new int[array.length][array.length];
+        
+        
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+            // moves from right to left
+//                doubleArray[i][j] = array[(i + j) % array.length];
+            // moves from felt to right
+                doubleArray[i][(i + j) % array.length] = array[j];
             }
-            System.out.println("");
-            
         }
-        // numbers move from right to left one number in a line
-//        for (int i = 0; i < doubleMasyvas.length; i++, offset--) {
-//            int[] line = doubleMasyvas[i];
-//            for (int j = 0; j < line.length; j++) {
-//                int temp = offset;
-//                if (temp + j < 0)
-//                    temp += line.length;
-//                    line[j] = masyvas[temp + j];
-//                    System.out.print(line[j]);
-//            }
-//            System.out.println("");
-//        }
+        for (int i = 0; i < doubleArray.length; i++) {
+            int[] line = doubleArray[i];
+            for (int j = 0; j < line.length; j++) {
+                int element = line[j];
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
     }
 }
